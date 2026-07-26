ce7d4063d9f97e5ee87d365bf6d5a4d5e0e3e9bd
hoangkha-lab <hoangkha2602@gmail.com>
2026-07-26 19:14:35 +0700

    update asm

--- NMLT-18-LyHoangKha-PS50045/LABS 4/MENU.C
+++ NMLT-18-LyHoangKha-PS50045/LABS 4/MENU.C
@@ -1,41 +1,68 @@
-#include <stdio.h> //khai bao thu vien
-
+#include <studio.h>
+#include <nmath.h>
 int main()
 {
-
-
-    int chon;
-    do
-    {
-        printf("+-------------------------------+\n");
-        printf("|  MENU CHUONG TRINH LAB 3.     |\n");
-        printf("+-------------------------------+\n");
-        printf("| 0. Thoat chuong trinh.        |\n");
-        printf("| 1. Tinh hoc luc sinh vien     |\n");
-        printf("| 2. Giai phuong trinh bac 2    |\n");
-        printf("| 3. Tinh tien dien             |\n");
-        printf("+-------------------------------+\n");
-        printf("Ban hay chon chuc nang (0 - 3): ");
-        scanf("%d", &chon); //chon :0
-        switch (chon)
-        {
-        case 0:
-            printf("Ban da chon chuc nang: 0. Thoat chuong trinh.\n");
-            break;
-        case 1:
-            printf("Ban da chon chuc nang: 1. Tinh hoc luc sinh vien.\n");
-            break;
-        case 2:
-            printf("Ban da chon chuc nang: 2. Giai phuong trinh bac 2.\n");
-            break;
-        case 3:
-            printf("Ban da chon chuc nang: 3. Tinh tien dien.\n");
-            break;
-        default:
-            printf("Ban phai chon chuc nang 0 - 3\n");
-            break;
-        }
-    } while (chon != 0); //0 != 0
-
-    return 0;
+int chon;
+do
 }
+        printf("-------------------------------------------------\n");
+        printf("|                 CHON CHUC NANG                |\n");
+        printf("-------------------------------------------------\n");
+        printf("| 0.  Thoat chuong trinh                        |\n");
+        printf("| 1.  Kiem tra so nguyen                        |\n");
+        printf("| 2.  Tim uoc so chung va so boi chung          |\n");
+        printf("| 3.  Tinh tien dien quan karaoke               |\n");
+        printf("| 4.  Tinh tien dien                            |\n");
+        printf("| 5.  Doi tien                                  |\n");
+        printf("| 6.  Tinh lai xuat vay ngan hang vay tra gop   |\n");
+        printf("| 7.  Vay tien mua xe                           |\n");
+        printf("| 8.  Sap xep thong tinh sinh vien              |\n");
+        printf("| 9.  Game Poly-LoTT                            |\n");
+        printf("| 10. Tinh toan phan so                         |\n");
+        printf("-------------------------------------------------\n");
+        printf("Nhap lua chon cua ban: ");
+        scanf("%d", &chon);
+        switch(chon)
+{
+case  0:
+{ 
+printf("Ong chu da chon chuc nang so 0. Thoat chuong trinh \n");
+break;
+}
+case 1:
+{
+printf(" Ong chu da chon chuc nang so 1. Moi ong chu nhap X \n");
+break;
+}
+case 2:
+{ 
+printf(" Ong chu da chon chuc nang so 2. Moi ong chu nhap X \n");
+break;
+}
+case 3:
+{
+printf(" Ong chu da chon chuc nang so 3. Moi ong chu nhap so Kw \n");
+break;
+}   
+case 4:
+{       
+printf(" Ong chu da chon chuc nang so 4. Moi ong chu nhap so tien dien  \n");
+break;
+}
+case 5:
+{       
+printf(" Ong chu da chon chuc nang so 5. Moi ong chu dat ten cho minh a  \n");
+break;
+}       
+case 6:
+{   
+printf(" Ong chu da chon chuc nang so 6. Moi ong chu nhap so tien da vay  \n");
+break;
+}   
+case 7:
+{   
+printf(" Ong chu da chon chuc nang so 7. Moi ong chu nhap so tien muon vay \n");
+break;
+}
+case 8:
+{

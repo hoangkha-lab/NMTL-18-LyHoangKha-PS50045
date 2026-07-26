d69bd76704135faf4730d4372951848f90f80c47
hoangkha-lab <hoangkha2602@gmail.com>
2026-07-26 19:43:56 +0700

    Update Bai3_Tinhdiem.c

--- NMLT-18-LyHoangKha-PS50045/LABS 1/Bai 3/Bai3_Tinhdiem.c
+++ NMLT-18-LyHoangKha-PS50045/LABS 1/Bai 3/Bai3_Tinhdiem.c
@@ -1,35 +1,41 @@
-#include <stdio.h> //khai bao thu vien
-int main () 
+#include <stdio.h>
+#include <string.h>
+#include <time.h> //chạy chương trình tính tuổi
+int main()
 {
-    char mssv[20]; //   khai bao bien mssv de luu tru ma so sinh vien      
-    char Hoten[50]; //   khai bao bien hoten de luu tru ho va ten sinh vien
-    float toan, ly, hoa, dTB; //   khai bao bien diemTB de luu tru diem trung binh cua sinh vien
-
-    printf("Nhap ma so sinh vien: ");
-    scanf("%19s", mssv);
-
-    getchar();
+    int toan, ly, hoa;
+    char HoVaTen[50];
+    char MSSV[10];
+    unsigned short Namsinh;
 
     printf("Nhap ho va ten: ");
-    fgets(Hoten, sizeof(Hoten), stdin);
+    scanf("%[^\n]", HoVaTen);
 
-    printf("Nhap diem Toan:");
-    scanf("%f", &toan);
-    printf("Nhap diem Ly:");
-    scanf("%f", &ly);
-    printf("Nhap diem Hoa:");
-    scanf("%f", &hoa);
+    printf("Nhap MSSV: ");
+    scanf("%s", MSSV);
+    getchar();
 
-    dTB = (toan*2+ly+hoa)/ (float)4; //tinh diem trung binh
+    printf("Nhap nam sinh: ");
+    scanf("%hu", &Namsinh);
 
-    printf("*KET QUA*\n");
-    printf("Ma so sinh vien: %s\n", mssv);
-    printf("Ho va ten: %s", Hoten);
-    printf("Diem trung binh: %.2f\n", dTB);
-   
-   {
-    system("pause");
+    printf("Nhap diem mon Toan: ");
+    scanf("%d", &toan);
+
+    printf("Nhap diem mon Ly: ");
+    scanf("%d", &ly);
+
+    printf("Nhap diem mon Hoa: ");
+    scanf("%d", &hoa);
+
+    printf("\nThong tin sinh vien:\n");
+    printf("Ho va ten: %s\n", HoVaTen);
+    printf("MSSV: %s\n", MSSV);
+    printf("Nam sinh: %hu\n", Namsinh);
+    printf("Tuoi: %d\n", 2026 - Namsinh);
+    printf("Diem Toan: %d\n", toan);
+    printf("Diem Ly: %d\n", ly);
+    printf("Diem Hoa: %d\n", hoa);
+    printf("Diem trung binh: %.2f\n", (toan + ly + hoa) / 3.0);
     return 0;
-   }
 
 }

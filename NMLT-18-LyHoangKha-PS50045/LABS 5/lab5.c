4ffd0ff8aedde331f36745bf7e96749c8b36c20b
hoangkha-lab <hoangkha2602@gmail.com>
2026-08-06 10:10:05 +0700

    Update lab5.c

--- NMLT-18-LyHoangKha-PS50045/LABS 5/lab5.c
+++ NMLT-18-LyHoangKha-PS50045/LABS 5/lab5.c
@@ -41,4 +41,14 @@ int main(){
     } while (chon != 5);
     
     return 0;
+}void menu(){
+    printf("+---------------------------------------------------+\n");
+    printf("|             MENU CHƯƠNG TRÌNH LAB 5               |\n");
+    printf("+---------------------------------------------------+\n");
+    printf("| 1. Tìm giá trị lớn nhất trong 3 số                |\n");
+    printf("| 2. Kiểm tra Năm nhuận                             |\n");
+    printf("| 3. Hoán vị 2 số (Sử dụng Con trỏ)                 |\n");
+    printf("| 4. Kiểm tra & Phân loại Tam giác                  |\n");
+    printf("| 5. Thoát chương trình                             |\n");
+    printf("+---------------------------------------------------+\n");
 }

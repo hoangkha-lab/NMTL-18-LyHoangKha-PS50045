615808e8f1b81f47022d1388519a75f335f7ffa5
hoangkha-lab <hoangkha2602@gmail.com>
2026-08-06 10:27:48 +0700

    commit

--- NMLT-18-LyHoangKha-PS50045/Lab6/Main_bai6.c
+++ NMLT-18-LyHoangKha-PS50045/Lab6/Main_bai6.c
@@ -1,85 +1,254 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <math.h>
+#include <stdbool.h>
+//khai báo các hàm chức năng
+void chucnang1();
+void chucnang2();
+void chucnang3();
+void chucnang4();
+void chucnang5();
 
-// --- NGUYÊN MẪU HÀM (FUNCTION PROTOTYPE) ---
-void timGiaTriLonNhat(void);
-void kiemTraNamNhuan(void);
-void hoanViHaiSo(void);
-void kiemTraPhanLoaiTamGiac(void);
+int main()
+{
+    int chon;
+    do
+    {
+        printf("+-------------------------------------------------------+\n");
+        printf("|  MENU CHUONG TRINH LAB 6.                             |\n");
+        printf("+-------------------------------------------------------+\n");
+        printf("| 1. Tinh trung binh tong so chia het cho 3 va 5        |\n");
+        printf("| 2. Tim gia tri lon nhat va nho nhat trong mang        |\n");
+        printf("| 3. Sap xep mang theo thu tu giam dan                  |\n");
+        printf("| 4. Ma tran binh phuong (mang 2 chieu)                 |\n");
+        printf("| 5. Loc va xuat vi tri cac so le trong ma tran         |\n");
+        printf("| 6. Thoat chuong trinh                                 |\n");
+        printf("+-------------------------------------------------------+\n");
+        printf("Ban hay chon chuc nang (1 - 6): ");
+        scanf("%d", &chon);
 
-// --- HÀM CHÍNH (MAIN) ---
-int main(void) {
-    int luaChon;
-
-    do {
-        // Giao diện Menu hiển thị
-        printf("+---------------------------------------+\n");
-        printf("|        MENU CHUONG TRINH LAB 5        |\n");
-        printf("+---------------------------------------+\n");
-        printf("| 1. Tim gia tri lon nhat trong 3 so    |\n");
-        printf("| 2. Kiem tra Nam nhuan                 |\n");
-        printf("| 3. Hoan vi 2 so (Su dung Con tro)     |\n");
-        printf("| 4. Kiem tra & Phan loai Tam giac      |\n");
-        printf("| 5. Thoat chuong trinh                 |\n");
-        printf("+---------------------------------------+\n");
-        printf(">> Xin moi chon chuc nang (1-5): ");
-        
-        // Đọc lựa chọn từ người dùng
-        scanf("%d", &luaChon);
-
-        // Điều khiển gọi các hàm chức năng bằng switch-case
-        switch (luaChon) {
+        switch (chon)
+        {
             case 1:
-                timGiaTriLonNhat();
+                printf("Tinh trung binh tong so chia het cho 3 va 5.\n");
+                chucnang1();
                 break;
             case 2:
-                kiemTraNamNhuan();
+                printf("Tim gia tri lon nhat va nho nhat trong mang.\n");
+                chucnang2();
                 break;
             case 3:
-                hoanViHaiSo();
+                printf("Sap xep mang theo thu tu giam dan.\n");
+                chucnang3();
                 break;
             case 4:
-                kiemTraPhanLoaiTamGiac();
+                printf("Ma tran binh phuong (mang 2 chieu).\n");
+                chucnang4();
                 break;
             case 5:
-                printf("\nCam on ban da su dung chuong trinh. Tam biet!\n");
+                printf("Loc va xuat vi tri cac so le trong ma tran.\n");
+                chucnang5();
+                break;
+            case 6:
+                printf("Thoat chuong trinh.\n");
                 break;
             default:
-                printf("\n[Loi] Lua chon khong hop le! Vui long chon lai tu 1 den 5.\n\n");
+                printf("Loi: Chuc nang khong hop le.\n");
+                printf("Ban hay chon tu 1 - 6.\n");
+                break;
         }
-        
-        // Thêm khoảng trống và tạm dừng để dễ quan sát sau mỗi chức năng
-        if (luaChon != 5) {
-            printf("\n---------------------------------------\n\n");
-        }
-
-    } while (luaChon != 5);
+    }
+    while (chon != 6);
 
     return 0;
 }
 
-// --- ĐỊNH NGHĨA CÁC HÀM CHI TIẾT (STUB/MÔ-ĐUN HÓA) ---
+//chức năng và hàm 
 
-// Chức năng 1
-void timGiaTriLonNhat(void) {
-    printf("\n--> Ban da chon: Tim gia tri lon nhat trong 3 so.\n");
-    // Sinh viên viết code xử lý chi tiết tại đây
+float tinhTrungBinh(int a[], int n)
+{
+    int tong = 0;
+    int sodem = 0;
+    for(int i = 0; i < n; i++){
+        if(a[i] % 3 == 0 && a[i] % 5 == 0)
+        {
+            tong += a[i];
+            sodem++;
+        }
+    }
+    if(sodem == 0)
+    {
+        return -__FLT_MAX__; // số nhỏ nhất kiểu float
+    }
+    else
+    {
+        return (float)tong / sodem;
+    }
 }
 
-// Chức năng 2
-void kiemTraNamNhuan(void) {
-    printf("\n--> Ban da chon: Kiem tra Nam nhuan.\n");
-    // Sinh viên viết code xử lý chi tiết tại đây
+void chucnang1()
+{
+    int n = 5;
+    int a[10];
+    printf("Nhap 5 so nguyen:\n");
+    scanf("%d",&n);
+    for(int i = 0; i < n; i++)
+    {
+        printf("Nhap a[%d] = ", i);
+        scanf("%d", &a[i]);
+    }
+    float trungBinh = tinhTrungBinh(a, n);
+    if(trungBinh == -__FLT_MAX__)
+    {
+        printf("Khong co so nao chia het cho 3 va 5 trong mang.\n");
+    }
+    else
+    {
+        printf("Trung binh tong cac so chia het cho 3 va 5 trong mang la: %.2f\n", trungBinh);
+    }
 }
 
-// Chức năng 3
-void hoanViHaiSo(void) {
-    printf("\n--> Ban da chon: Hoan vi 2 so (Su dung Con tro).\n");
-    // Sinh viên viết code xử lý chi tiết tại đây
+// xg chức năng 1
+void timMaxMin(int a[], int n)
+{
+    int min,max;
+    min = a[0];
+    max = a[0];
+    for(int i = 1; i < n; i++)
+    {
+        if (min > a[i])
+        {
+            min = a[i];
+        }
+        if (max < a[i])
+        {
+            max = a[i];
+        }
+    }
+    printf("Gia tri nho nhat trong mang la: %d\n", min);
+    printf("Gia tri lon nhat trong mang la: %d\n", max);
+    
 }
+void chucnang2()
+{
+    int n = 5;
+    int a[10];
+    printf("Nhap 5 so nguyen:\n");
+    scanf("%d",&n);
+    for(int i = 0; i < n; i++)
+    {
+        printf("Nhap a[%d] = ", i);
+        scanf("%d", &a[i]);
+    }
+    timMaxMin(a, n);
+}
+//xog chức năng 2
 
-// Chức năng 4
-void kiemTraPhanLoaiTamGiac(void) {
-    printf("\n--> Ban da chon: Kiem tra & Phan loai Tam giac.\n");
-    // Sinh viên viết code xử lý chi tiết tại đây
+//chức năng 3
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
+void sapXepGiamDan(int a[], int n)
+{
+    for(int i = 0; i < n - 1; i++)
+    {
+        for(int j = i + 1; j < n; j++)
+        {
+            if(a[i] < a[j])
+            {
+                swap(&a[i], &a[j]);
+            }
+        }
+    }
+    for(int i = 0; i < n; i++)
+    {
+        printf("%d \n", a[i]);
+    }
+}
+void chucnang3()
+{
+    int n = 5;
+    int a[10];
+    printf("Nhap 5 so nguyen:\n");
+    scanf("%d",&n);
+    for(int i = 0; i < n; i++)
+    {
+        printf("Nhap a[%d] = ", i);
+        scanf("%d", &a[i]);
+    }
+    printf("\n");
+    sapXepGiamDan(a, n);    
+}
+// xog chức năng 3
+//chuc năng 4
+void maTranBinhPhuong(int row, int col)
+{
+    int a[row][col];
+    //nhap
+    for(int i = 0; i < row; i++)
+    {
+        for(int j = 0; j < col; j++)
+        {
+            printf("Nhap a[%d][%d] = ", i, j);
+            scanf("%d", &a[i][j]);
+        }
+    }
+    //xuat
+    for(int i = 0; i < row; i++)
+    {
+        for(int j = 0; j < col; j++)
+        {
+            printf("%3d \t", a[i][j] * a[i][j]);
+        }
+        printf("\n");
+    }
+} 
+void chucnang4()
+{
+    int row, col;
+    printf("Nhap so hang: ");
+    scanf("%d", &row);
+    printf("Nhap so cot: ");
+    scanf("%d", &col);
+    maTranBinhPhuong(row, col);
+}
+//xog chức năng 4
+
+//chức năng 5
+void xuatSoLeMaTran(int row, int col)
+{
+    int a[row][col];
+    //nhap
+    for(int i = 0; i < row; i++)
+    {
+        for(int j = 0; j < col; j++)
+        {
+            printf("Nhap a[%d][%d] = ", i, j);
+            scanf("%d", &a[i][j]);
+        }
+    }
+    //xuat vi tri so le
+    printf("Vi tri cac so le trong ma tran:\n");
+    for(int i = 0; i < row; i++)
+    {
+        for(int j = 0; j < col; j++)
+        {
+            if(a[i][j] % 2 != 0)
+            {
+                printf("a[%d][%d] = %d\n", i, j, a[i][j]);
+            }
+        }
+    }
+}
+void chucnang5()
+{
+    int row, col;
+    printf("Nhap so hang: ");
+    scanf("%d", &row);
+    printf("Nhap so cot: ");
+    scanf("%d", &col);
+    xuatSoLeMaTran(row, col);
+}
+//xog chức năng 5 

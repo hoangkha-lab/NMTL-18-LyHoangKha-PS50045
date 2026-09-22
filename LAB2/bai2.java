cad217e9f6a839849cffb658809a85128cb19fde
hoangkha-lab <hoangkha2602@gmail.com>
2026-09-22 10:43:27 +0700

    commit

---
+++ LAB2/bai2.java
@@ -0,0 +1,10 @@
+package LAB2;
+public class bai2 {
+    public static  void main(String[] args){
+int dk = 9;
+while (dk < 10)
+{
+    System.out.println(dk + " kha handsome");
+}
+    }
+}
---
+++ LAB2/bai4.java
@@ -0,0 +1,43 @@
+package LAB2;
+
+import java.util.Scanner;
+
+public class bai4 {
+    public static void main(String[] args) {
+        Scanner nhap = new Scanner(System.in);
+
+        System.out.println("Nhap a: ");
+        int a = nhap.nextInt();
+        System.out.println("Nhap b: ");
+        int b = nhap.nextInt();
+
+        System.out.println("Nhap (+, -, *, /): ");
+        char phepToan = nhap.next().charAt(0);
+        switch (phepToan) {
+            case '+':
+                System.out.println("phep cong");
+                System.out.println("Ket qua: " + (a + b));
+                break;
+            case '-':
+                System.out.println("phep tru");
+                System.out.println("Ket qua: " + (a - b));
+                break;
+            case '*':
+                System.out.println("phep nhan");
+                System.out.println("Ket qua: " + (a * b));
+                break;
+            case '/':
+                System.out.println("phep chia");
+                if (b == 0) {
+                    System.out.println("Khong the chia cho 0");
+                    return;
+                } else {
+                    System.out.println("Ket qua: " + (a / b));
+                }
+
+                break;
+            default:
+                System.out.println("Phep toan khong hop le");
+        }
+    }
+}
